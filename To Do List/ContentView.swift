32226e7600fcd0ac71fe3df07473a85a07a89d66
Xiao Yuan Lv <xiaoyuanlv.31@gmail.com>
2021-01-12 17:19:45 +0630

    First Commit

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var newToDo = ""
+    @State private var allTodos : [TodoItem] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Add todo ...", text: $newToDo).textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button(action: {
+                        guard !self.newToDo.isEmpty else { return }
+                        self.allTodos.append(TodoItem(todo: self.newToDo))
+                        self.newToDo = ""
+                        self.saveTodos()
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding(.leading, 5)
+                }.padding()
+                
+                List {
+                    ForEach(allTodos) { todoItem in
+                        Text(todoItem.todo)
+                    }.onDelete(perform: deleteTodo)
+                }
+                
+            }
+            .navigationTitle("Todos")
+        }.onAppear(perform: loadTodos)
     }
+    
+    private func deleteTodo(at offsets: IndexSet) {
+        self.allTodos.remove(atOffsets:  offsets)
+        saveTodos()
+    }
+    
+    private func loadTodos() {
+        if let todosData  = UserDefaults.standard.value(forKey: "todosKey") as? Data {
+            if let todosList = try? PropertyListDecoder().decode(Array<TodoItem>.self, from: todosData) {
+                self.allTodos = todosList
+            }
+        }
+    }
+    
+    private func saveTodos() {
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(self.allTodos), forKey: "todosKey")
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct TodoItem: Codable, Identifiable {
+    var id = UUID()
+    let todo: String
 }
